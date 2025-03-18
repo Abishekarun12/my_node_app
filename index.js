faddf4226f29bf5c868c4775c2a406d3835c0cbe
Abishek Arunachalam <abishek@cloudrevelinnovation.com>
2025-03-18 18:20:57 +0530

    node cowsay "npm i cowsay"

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 console.log("Hi This is from Index.js");
 console.log("Hi This is a second Line");
-process.exit(0);
+// process.exit(0);
+
+
+// ------------Cowsay-------------
+// To the Terminal: "npm cowsay install"
+
+const cowsay = require('cowsay');
+console.log(cowsay.say({
+    text: "Hello World!",
+    e: "oO",
+    T: "V "
+}));
